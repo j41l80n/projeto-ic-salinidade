520a02339140d7aea2cef1a87dc73d4c02655467
jailson santos <jailson_donald_cobain@hotmail.com>
2018-06-19 10:47:24 -0300

    codigo para medir a condutividade

--- salinidade/salinidade.ino
+++ salinidade/salinidade.ino
@@ -1,30 +1,18 @@
-float aRef = 5;
-float relacaoA0 = 11;
-float relacaoA2 = 11;
-float tensaoA0;
-#define AMOSTRAS 12
+#include <SPI.h>
+
+const int switchPin = 6;
+int switchState = 0;
+int condVal;
 
 void setup() {
   Serial.begin(9600);
-}
-
-float lePorta(uint8_t portaAnalogica) {
-  float total = 0;
-  for (int i = 0; i < AMOSTRAS; i++) {
-    total += 1.0 * analogRead(portaAnalogica);
-    delay(5);
-  }
-  return total / (float)AMOSTRAS;
-}
-
-void mostraTensoes() {
-  Serial.print("Tensão em A0: ");
-  Serial.print(tensaoA0 * relacaoA0);
-  Serial.println ("V");
+  SPI.begin();
+  pinMode(switchPin, INPUT);
 }
 
 void loop() {
-  tensaoA0 = (lePorta(A0) * aRef) / 1024.0;
-  mostraTensoes();
-  delay(500);
+  condVal = analogRead(A0);
+  float voltage = condVal * (5.0 / 1023.0); //calcula a condutividade
+  Serial.print("Valor da tensão :");
+  delay(50); //delay before looping
 }
